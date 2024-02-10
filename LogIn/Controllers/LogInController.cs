7809b6fe612e95fb891c35fe1a2458602a5e77f3
Snehasish Mallik <malliksnehasish560@gmail.com>
2024-02-10 23:42:34 +0530

    Your commit message here

---
+++ LogIn/Controllers/LogInController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LogIn.Models;
+
+namespace LogIn.Controllers
+{
+    public class LogInController : Controller
+    {
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult LogIn(LogInModel model)
+        {
+            if (model.type == "User" && model.password=="user1@123")
+            {
+                return RedirectToAction("UserPage", model);
+            }
+            else if(model.type == "Admin")
+            {
+                return RedirectToAction("AdminPage", model);
+            }
+            return RedirectToAction("LogIn");
+        }
+
+        public IActionResult UserPage(LogInModel model)
+        {
+            return View(model);
+        }
+
+        public IActionResult AdminPage(LogInModel model)
+        {
+            return View(model);
+        }
+
+
+    }
+}
---
+++ LogIn/Models/LogInModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LogIn.Models
+{
+    public class LogInModel
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public string type { get; set; }//admin or user
+    }
+}
